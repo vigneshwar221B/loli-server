578aefe2f88fd660bae4dd9d15ed7fee511098a4
Vignesh War <vigneshwar@Vigneshs-MacBook-Pro.local>
2019-12-21 17:40:29 +0530

    supports electron

--- app.js
+++ app.js
@@ -42,6 +42,20 @@ app.get('/supersecretroute', (req, res) => {
 	res.send(data)
 })
 
+app.get('/supersecretrouteapp', (req, res) => {
+	let data = fs.readFileSync('app-log.txt').toString()
+	res.send(data)
+})
+
+app.post('/app', (req, res) => {
+	console.log(req.body)
+
+	fs.appendFileSync('app-log.txt', JSON.stringify(req.body, null, 2), err => {
+		if (err) console.log(err)
+		else console.log('it worked!')
+	})
+})
+
 app.listen(process.env.PORT || 8000, () => {
 	console.log('server started at 8000')
 })
